8e81a5f10c57c71f6b6e54f1ed042e7c69cbcdb1
gonzalo ruiz <38412697+gonzaruiz@users.noreply.github.com>
2018-08-15 12:12:51 +0800

    Add files via upload

---
+++ Assigment18.2.py
@@ -0,0 +1,23 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+P1 = Republican (first state)
+P2 = Republican (second state) 
+p1 = sample in the first state 
+p2 = sample in the second state 
+The number sampled (n1) = 100 
+The number sampled (n2) = 100
+Mean of the difference of sample proportions: E(p1 - p2) = P1 - P2 = 0.52 - 0.47 = 0.05.
+
+SD= sq{[P1(1 - P1)/n1] + [P2(1 - P2) / n2]}
+SD= sq{[(0.52)(0.48)/100] + [ (0.47)(0.53) / 100]}
+SD= sq(0.002496+0.002491) = sq(0.004987) = 0.0706
+finding p1  < p2 or (p1 - p2) < 0, finding z-score of p1 - p2
+z(p1-p2) = (x - μ(p1-p2)) / SD =(0 - 0.05)/0.0706 = -0.7082
+z-score being -0.7082 or less=0.24. 
+Resulting in show a greater percentage of Republican voters in the second state than the first state of 24%
+ 
+
